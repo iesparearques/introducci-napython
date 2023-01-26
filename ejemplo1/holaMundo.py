8cc9b051a124704741cdda7603220b31d12e5c02
Arturo Candela <a.candelamolto@edu.gva.es>
2023-01-26 18:34:01 +0100

    Initial Commit

---
+++ ejemplo1/holaMundo.py
@@ -0,0 +1,19 @@
+# Imprimir Enteros
+
+entero1 = 1
+entero2 = 2
+ 
+entero3 = entero1 + entero2
+
+print(entero3)
+print(type(entero3))
+
+# Frases
+
+mensaje = "Hola "
+nombre = "Arturo"
+
+frase = mensaje + nombre
+
+print(frase)
+print(type(frase))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Introducción a Python
+
+En este repositorio voy a intentar colgar todo el código de python que utilicemos para trabajar en el instituto. 
+
+Estará en constante evolución.
